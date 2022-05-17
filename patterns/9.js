e6a57376d74751e4cfa0e8b81d0afb54e510ab8d
Anshul Dubey <anshulit38@gmail.com>
2022-05-17 12:32:24 +0530

    update file

--- patterns/9.js
+++ patterns/9.js
@@ -11,25 +11,25 @@
     *
 
  */
-let n= 10;
+let n= 5;
 for(let i =0; i<n; i++){
 let str = '';
     for(j=i; j<n-1; j++){
-        str = str + '*';
+        str = str + ' ';
     }
 
     for(k=-1; k<=2*i-1; k++){
-        str =str + '+';
+        str =str + '*';
     }
 console.log(str);
 }
 for(let l =0; l<n-1; l++){
     let str = "";
     for(let m=0; m<=l; m++){
-        str = str + '*';
+        str = str + ' ';
     }
     for(let p=l; p<2*n-l-3; p++){
-     str = str + '+';   
+     str = str + '*';   
     }
 
     console.log(str)
